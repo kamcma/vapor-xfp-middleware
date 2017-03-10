0536d5e68043320b920400e72757a185cc630718
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-09 22:17:27 -0500

    Make initializer publicly accessible

--- Sources/EnforceHerokuPiggybackSSL/EnforceHerokuPiggybackSSL.swift
+++ Sources/EnforceHerokuPiggybackSSL/EnforceHerokuPiggybackSSL.swift
@@ -2,6 +2,7 @@ import HTTP
 import URI
 
 public struct EnforceHerokuPiggybackSSL: Middleware {
+    public init() { }
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         if request.headers["X-Forwarded-Proto"] == "https" {
             return try next.respond(to: request)

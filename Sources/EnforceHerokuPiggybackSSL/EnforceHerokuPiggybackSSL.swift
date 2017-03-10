b5a2768f48bc0154d4f8a96a5bd94659cbcd9991
Kyle McMahon <kyle.mcmahon@mac.com>
2017-03-09 22:08:45 -0500

    Try new method of URI modification

--- Sources/EnforceHerokuPiggybackSSL/EnforceHerokuPiggybackSSL.swift
+++ Sources/EnforceHerokuPiggybackSSL/EnforceHerokuPiggybackSSL.swift
@@ -6,15 +6,7 @@ public struct EnforceHerokuPiggybackSSL: Middleware {
         if request.headers["X-Forwarded-Proto"] == "https" {
             return try next.respond(to: request)
         }
-        let uri = URI(
-            scheme: "https",
-            userInfo: request.uri.userInfo,
-			port: request.uri.port,
-			path: request.pathWithoutTrailingSlash,
-			query: request.uri.query,
-			rawQuery: request.uri.rawQuery,
-			fragment: request.uri.fragment
-        )
-        return Response(redirect: uri.description)
+        request.uri.scheme = "https"
+        return Response(redirect: request.uri.description)
     }
 }

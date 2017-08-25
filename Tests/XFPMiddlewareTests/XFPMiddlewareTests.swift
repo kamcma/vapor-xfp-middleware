2462121541201a3ea03395f7b2959f10bf6bdc4b
Kyle McMahon <kyle.mcmahon@mac.com>
2017-08-24 20:26:57 -0400

    Lint

--- Tests/XFPMiddlewareTests/XFPMiddlewareTests.swift
+++ Tests/XFPMiddlewareTests/XFPMiddlewareTests.swift
@@ -8,34 +8,33 @@ class XFPMiddlewareTests: XCTestCase {
     static var allTests = [
         ("testXFPMiddleware", testXFPMiddleware)
     ]
-    
+
     override func setUp() {
         Testing.onFail = XCTFail
     }
 
     func testXFPMiddleware() throws {
         let drop = try Droplet()
-        
+
         let protected = drop.grouped(XFPMiddleware.init())
-        
-        protected.get { req in
+
+        protected.get { _ in
             return Response(status: .ok)
         }
-        
+
         let httpReq = Request(method: .get, uri: "/")
         httpReq.uri.scheme = "http"
         httpReq.headers["X-Forwarded-Proto"] = "http"
-        
+
         let httpsReq = Request(method: .get, uri: "/")
         httpsReq.uri.scheme = "http"
         httpsReq.headers["X-Forwarded-Proto"] = "https"
-        
+
         try drop.testResponse(to: httpReq)
             .assertStatus(is: .seeOther)
-        
+
         try drop.testResponse(to: httpsReq)
             .assertStatus(is: .ok)
-        
-        
+
     }
 }

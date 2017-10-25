7b4d523afa7d33b9e2927bb745bc27f09c862f63
Kyle McMahon <kyle.mcmahon@mac.com>
2017-10-25 19:20:47 -0400

    Update to Swift 4

--- Package.swift
+++ Package.swift
@@ -1,8 +1,17 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "VaporXFPMiddleware",
+    products: [
+        .library(name: "VaporXFPMiddleware", targets: ["XFPMiddleware"])
+    ],
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2)
+        .package(url: "https://github.com/vapor/vapor.git", from: "2.3.0")
+    ],
+    targets: [
+        .target(name: "XFPMiddleware", dependencies: ["Vapor"]),
+        .testTarget(name: "XFPMiddlewareTests", dependencies: ["XFPMiddleware", "Testing"])
     ]
 )
